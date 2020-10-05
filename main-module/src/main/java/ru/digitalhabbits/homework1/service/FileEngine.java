b2f99e6d9c36451ad6bed05f8624c52316d5186a
Varvara <Vintentas@gmail.com>
2020-10-05 21:35:31 +0300

    Added changed to FileEngine.java:  - Add save to file

--- main-module/src/main/java/ru/digitalhabbits/homework1/service/FileEngine.java
+++ main-module/src/main/java/ru/digitalhabbits/homework1/service/FileEngine.java
@@ -2,7 +2,12 @@ package ru.digitalhabbits.homework1.service;
 
 import javax.annotation.Nonnull;
 import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
+
+import static java.lang.String.format;
 import static java.util.Arrays.stream;
 
 public class FileEngine {
@@ -10,14 +15,28 @@ public class FileEngine {
     private static final String RESULT_DIR = "results";
     private static final String RESULT_EXT = "txt";
 
+    private final String currentDir = System.getProperty("user.dir");
+    private final File resultDir = new File(currentDir + "/" + RESULT_DIR);
+
     public boolean writeToFile(@Nonnull String text, @Nonnull String pluginName) {
-        // TODO: NotImplemented
-        return true;
+        // TODO: NotImplemented -> done
+        //Создать директорию, если ее нет
+        resultDir.mkdirs();
+
+        //путь к файлу
+        String fileName = format((resultDir + "/" + RESULT_FILE_PATTERN), pluginName);
+
+        //запись в файл
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(fileName))) {
+            out.write(text);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public void cleanResultDir() {
-        final String currentDir = System.getProperty("user.dir");
-        final File resultDir = new File(currentDir + "/" + RESULT_DIR);
         stream(resultDir.list((dir, name) -> name.endsWith(RESULT_EXT)))
                 .forEach(fileName -> new File(resultDir + "/" + fileName).delete());
     }
--- main-module/src/main/java/ru/digitalhabbits/homework1/service/WikipediaClient.java
+++ main-module/src/main/java/ru/digitalhabbits/homework1/service/WikipediaClient.java
@@ -4,10 +4,14 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import org.apache.http.client.utils.URIBuilder;
 import javax.annotation.Nonnull;
-import java.io.*;
-import java.net.*;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.HttpURLConnection;
+
 import java.util.Map;
 import java.util.Set;
 
