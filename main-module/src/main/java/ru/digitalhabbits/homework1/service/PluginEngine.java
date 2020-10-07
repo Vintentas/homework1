f550a04a5097c2110643d7cbaa1548dc5d0f8df6
Varvara <Vintentas@gmail.com>
2020-10-07 19:40:37 +0300

    Added changed to PluginEngine.java

--- main-module/src/main/java/ru/digitalhabbits/homework1/service/PluginEngine.java
+++ main-module/src/main/java/ru/digitalhabbits/homework1/service/PluginEngine.java
@@ -3,12 +3,34 @@ package ru.digitalhabbits.homework1.service;
 import ru.digitalhabbits.homework1.plugin.PluginInterface;
 
 import javax.annotation.Nonnull;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class PluginEngine {
 
     @Nonnull
     public  <T extends PluginInterface> String applyPlugin(@Nonnull Class<T> cls, @Nonnull String text) {
-        // TODO: NotImplemented
-        return "NotImplemented";
+        // TODO: NotImplemented -> done
+        String result = "";
+
+        try {
+            //найдем метод
+            Method methodApply = cls.getMethod("apply", String.class);
+            //создадим новый объект
+            PluginInterface pluginInterface = cls.newInstance();
+            //вызовем метод на новом объекте
+            result = (String) methodApply.invoke(pluginInterface, text);
+
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
